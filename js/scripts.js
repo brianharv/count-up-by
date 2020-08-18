9180911f7e531e05bce4a854c2ce87b5577c4089
Brian Harvey <brian.harv3y@gmail.com>
2020-08-18 10:44:28 -0700

    build functioning user interface logic
    Co-authored-by: David Cole <davidtcole@yahoo.com>

--- js/scripts.js
+++ js/scripts.js
@@ -6,8 +6,12 @@ $(document).ready(function() {
   $("#formOne").submit(function(event) {
     event.preventDefault();
 
-    let userCountTo = $("input#input1").val();
-    let userCountBy = $("input#input2").val();
+    let userCountTo = parseInt($("input#input1").val());
+    let userCountBy = parseInt($("input#input2").val());
 
+    for (let index = userCountBy; index <=userCountTo; index += userCountBy) {
+      console.log(index);
+      $("#output").append("<li>" + index + "</li>");
+    }
   });
 });
