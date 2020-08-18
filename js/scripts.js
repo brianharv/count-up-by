97e584fb308e31a43debf9d33c9e26fb62a2cec5
Brian Harvey <brian.harv3y@gmail.com>
2020-08-18 10:32:52 -0700

    add basic html and user and business logic
    Co-authored-by: David Cole <davidtcole@yahoo.com>

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+//Business Logic
+
+
+//User Interface Logic
+$(document).ready(function() {
+  $("#formOne").submit(function(event) {
+    event.preventDefault();
+
+    let userCountTo = $("input#input1").val();
+    let userCountBy = $("input#input2").val();
+
+  });
+});
